26f678ffa501243b1367249a200aec0253309f2e
APT3233 <khacanh19803535@gmail.com>
2024-09-30 23:30:04 +0700

    update base sql

--- src/main/java/com/fitness/App.java
+++ src/main/java/com/fitness/App.java
@@ -1,5 +1,12 @@
 package com.fitness;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,10 +23,37 @@ public class App extends Application
         stage.setScene(scene);
         stage.show();
     }
+    public static void testDBConnection() {
+        Properties properties = new Properties();
+        try (InputStream input = App.class.getResourceAsStream("/application.properties")) {
+            properties.load(input);
+
+            String driver = properties.getProperty("db.driver");
+            String url = properties.getProperty("db.url");
+            String user = properties.getProperty("db.user");
+            String password = properties.getProperty("db.password");
+
+            Class.forName(driver);
+
+            try (Connection connection = DriverManager.getConnection(url, user, password)) {
+                if (connection != null) 
+                    System.out.println("Connected to the database!");
+                else 
+                    System.out.println("Failed to make connection!");
+                
+            } catch (SQLException e) {
+                System.out.println("SQL error: " + e.getMessage());
+            }
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error loading properties or driver: " + e.getMessage());
+        }
+    }
 
 
     public static void main( String[] args )
     {
+        testDBConnection();
         launch(args);
     }
 }
