a83950f9c2f6883f45b1314f3f51e1e3f5fea6fc
APT3233 <khacanh19803535@gmail.com>
2024-10-12 14:18:11 +0700

    3 new func check input

---
+++ src/main/java/com/fitness/utility/UtilityIO.java
@@ -0,0 +1,50 @@
+package com.fitness.utility;
+
+/**
+ * UtilityIO
+ */
+public class UtilityIO {
+
+    /*
+     * SignUp Page
+     * 
+     * text: string cần check 
+     * Oke -> return String
+     * No -> return null
+     * 
+     * NOTE: Ko sử dụng LOOP
+     */
+    public static String checkUserName(String text, String err)
+    {
+        try {
+
+            // implement
+            // use regex
+            return text;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /*
+     * Pass phải đủ 8 ký tự, số và chữ cái
+     * Chữ hoa, ký tự đặc biệt không bắt buộc
+     */
+    public static String checkPassWD(String text, String err)
+    {
+        // implement
+        return null;
+    }
+
+    /**
+     * @param Email phải đủ điều kiện là *@fpt.edu.vn và *@gmail.com 
+     *
+     *  Chỉ nhận 2 domain mail này              
+     */
+    public static String checkEmail(String text, String err) 
+    {
+        // implement
+        return null;
+    }
+
+}
